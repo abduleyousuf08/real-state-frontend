6a6b1c8220fbcb7e8de3278398666c61479f9b76
abdule yousuf <Abdule Yousuf Hashi>
2023-02-19 22:44:24 +0300

    updated the main content

--- src/Components/Content.js
+++ src/Components/Content.js
@@ -2,43 +2,59 @@ import "../App.css";
 import { GrFormNext } from "react-icons/gr";
 import { GrFormPrevious } from "react-icons/gr";
 import { MdTabUnselected } from "react-icons/md";
-
+import { MdOutlineBedroomParent } from "react-icons/md";
+import { MdOutlineBathroom } from "react-icons/md";
+import { ImLocation } from "react-icons/im";
 function Content() {
   return (
     <div>
       <div className="background">
         {/*this will be scrolling part click*/}
+
         <div className="scroll-buttons">
-          <a href="#">
-            <GrFormPrevious />
-          </a>{" "}
-          <a href="#">
-            {" "}
-            <GrFormNext />
-          </a>
+          <div className="button-one">
+            <a href="#">
+              <GrFormPrevious />
+            </a>{" "}
+          </div>
+          <div className="button-two">
+            <a href="#">
+              {" "}
+              <GrFormNext />
+            </a>
+          </div>
         </div>
 
         {/*the property information part*/}
 
         <div className="property-info">
           {/*the location of that house and the common district */}
-          <h1>West Broadway street</h1>
-          <p>42 jigjigayar street 10</p>
+          <a href="#" className="main-location">
+            <h1>West Broadway street</h1>
+          </a>
+          <p>
+            {" "}
+            <ImLocation className="icon-location" />
+            42 jigjigayar street 10
+          </p>
           {/*the propert information will be flex */}
           <div className="information-house">
-            <div>
+            <div className="prop-one">
+              <MdTabUnselected className="icon-one" />{" "}
               <p>
                 {" "}
                 <span>170</span> <br />
                 Square fit
               </p>
             </div>
-            <div>
+            <div className="prop-two">
+              <MdOutlineBedroomParent className="icon-two" />
               <p>
                 <span>1</span> <br /> Bedroom
               </p>
             </div>
-            <div>
+            <div className="prop-three">
+              <MdOutlineBathroom className="icon-three" />
               <p>
                 {" "}
                 <span>1</span> <br />
