0eec7295f7657ef6b33fd7ca769c6691b9152ed7
abdule yousuf <Abdule Yousuf Hashi>
2023-02-19 22:11:43 +0300

    updated real-state-frontend

---
+++ src/Components/Content.js
@@ -0,0 +1,57 @@
+import "../App.css";
+import { GrFormNext } from "react-icons/gr";
+import { GrFormPrevious } from "react-icons/gr";
+import { MdTabUnselected } from "react-icons/md";
+
+function Content() {
+  return (
+    <div>
+      <div className="background">
+        {/*this will be scrolling part click*/}
+        <div className="scroll-buttons">
+          <a href="#">
+            <GrFormPrevious />
+          </a>{" "}
+          <a href="#">
+            {" "}
+            <GrFormNext />
+          </a>
+        </div>
+
+        {/*the property information part*/}
+
+        <div className="property-info">
+          {/*the location of that house and the common district */}
+          <h1>West Broadway street</h1>
+          <p>42 jigjigayar street 10</p>
+          {/*the propert information will be flex */}
+          <div className="information-house">
+            <div>
+              <p>
+                {" "}
+                <span>170</span> <br />
+                Square fit
+              </p>
+            </div>
+            <div>
+              <p>
+                <span>1</span> <br /> Bedroom
+              </p>
+            </div>
+            <div>
+              <p>
+                {" "}
+                <span>1</span> <br />
+                Bathroom
+              </p>
+            </div>
+          </div>
+          {/*sale button */}
+          <button className="sale-btn">Sale</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Content;
---
+++ src/Components/Header.js
@@ -0,0 +1,41 @@
+import logo from "../images/updated.png";
+import "../App.css";
+import { FaLock } from "react-icons/fa";
+import { MdOutlineCreate } from "react-icons/md";
+
+function Header() {
+  return (
+    <div>
+      <div className="container">
+        <div className="logo">
+          <img src={logo} alt="logo" />
+        </div>
+
+        <div className="sub-container">
+          <div className="first-items">
+            <a href="#">Home</a>
+            <a href="#">About us</a>
+            <a href="#">Categories</a>
+          </div>
+          <div className="second-items">
+            <a href="#" className="sign-in">
+              <FaLock /> Sign in
+            </a>
+            <a href="#" className="submit">
+              {" "}
+              <span className="icon-submit">
+                {" "}
+                <MdOutlineCreate />
+              </span>{" "}
+              Submit property{" "}
+            </a>
+          </div>
+        </div>
+      </div>
+
+      {/*content part */}
+    </div>
+  );
+}
+
+export default Header;
